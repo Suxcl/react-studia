22f45f48aa51df51bb51f92dd06c16d0edada2b3
muzykos <77461733+muzykos@users.noreply.github.com>
2024-01-16 10:47:55 +0100

    Mały fix
    idk

--- my-app/src/components/datFetch/usersGet.tsx
+++ my-app/src/components/datFetch/usersGet.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function DataFetchingComponent() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<any[]>([]);
 
   useEffect(() => {
     fetchData();

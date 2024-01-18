c0767bf4b74ad2cc25d7a4e44a668c448bab300f
muzykos <77461733+muzykos@users.noreply.github.com>
2024-01-18 17:52:00 +0100

    Quick navbar :>

--- my-app/src/pages/layout.tsx
+++ my-app/src/pages/layout.tsx
@@ -1,31 +1,45 @@
 import { Outlet, Link } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
 
 const Layout = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/blog">Home</Link>
-          </li>
-          <li>
-            <Link to="/test">Test</Link>
-          </li>
-          <li>
-            <Link to="/">Profile</Link>
-          </li>
-          <li>
-            <Link to="/contact">Login</Link>
-          </li>
-          <li>
-            <Link to="/contact">Register</Link>
-          </li>
-        </ul>
-      </nav>
-
+      <AppBar position="static" sx={{ bgcolor: "#9b5de5" }}>
+        
+        <Toolbar>
+          <Link to="/blog">
+            <Button color="inherit" variant="contained" sx={{ mr: 2 }}>
+              Home
+            </Button>
+          </Link>
+          <Link to="/test">
+            <Button color="inherit" variant="contained" sx={{ mr: 2 }}>
+              Test
+            </Button>
+          </Link>
+          <Link to="/">
+            <Button color="inherit" variant="contained" sx={{ mr: 2 }}>
+              Profile
+            </Button>
+          </Link>
+          <Link to="/contact">
+            <Button color="inherit" variant="contained" sx={{ mr: 2 }}>
+              Login
+            </Button>
+          </Link>
+          <Link to="/contact">
+            <Button color="inherit" variant="contained">
+              Register
+            </Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
       <Outlet />
     </>
-  )
+  );
 };
 
 export default Layout;

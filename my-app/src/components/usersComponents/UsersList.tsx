6c62a1d41798a7092d6a7715e891cf198652360e
Sak <jacubsak@gmail.com>
2024-01-28 20:08:57 +0100

    seartch bar added

--- my-app/src/components/usersComponents/UsersList.tsx
+++ my-app/src/components/usersComponents/UsersList.tsx
@@ -1,18 +1,32 @@
 import { Link } from "react-router-dom"
 import { User } from "../../types/user"
 import UserC from "./UserC"
-import { List, ListItem, Paper, Typography } from '@mui/material';
+import { List, ListItem, Paper, TextField, Typography } from '@mui/material';
 
 import PersonIcon from '@mui/icons-material/Person';
+import { useState } from "react";
 
 function UsersList(props: {usersList: User[]}) {
-
+  
+  const [searchQuery, setSearchQuery] = useState('');
   const usersList = props.usersList
     
+
+  const filteredUsers = usersList.filter(user =>
+    user.username.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    user.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    user.surname.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
+
   return (
     <Paper elevation={3} style={{ padding: '1rem' }}>
+        <TextField label="Search Users" variant="outlined" fullWidth value={searchQuery} onChange={handleSearchChange} style={{ marginBottom: '1rem' }}/>
       <List>
-        {usersList.map((user) => (
+        {filteredUsers.map((user) => (
           <ListItem key={user.id}>
             <Link to={`/Profile/${user.id}`} style={{ textDecoration: 'none' }}>
               <Typography>

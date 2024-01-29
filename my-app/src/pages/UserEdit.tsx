9f069f67e8d177dc8be92d77686f51ea4d3138ef
Sak <107257050+Suxcl@users.noreply.github.com>
2024-01-29 16:47:13 +0100

    asd

--- my-app/src/pages/UserEdit.tsx
+++ my-app/src/pages/UserEdit.tsx
@@ -3,7 +3,7 @@ import { useLocation} from "react-router-dom";
 import UserForm from '../components/usersComponents/UserForm';
 
 function UserEdit(){
-
+	document.title = "User edit Page";
 	const {state} = useLocation()
 
 	return (
